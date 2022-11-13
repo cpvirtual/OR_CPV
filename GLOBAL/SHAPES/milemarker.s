b0852caf56132ea7927f09a4b6fe91498472844c
CPV <cpvirtual@gmail.com>
2022-11-13 11:23:20 +0000

    add Global

---
+++ GLOBAL/SHAPES/milemarker.s
@@ -0,0 +1,184 @@
+SIMISA@@@@@@@@@@JINX0s1t______
+
+shape (
+	shape_header ( 00000000 00000000 )
+	volumes ( 2
+		vol_sphere (
+			vector ( 0 2.2634 804.672 ) 844.922
+		)
+		vol_sphere (
+			vector ( 0 2.2634 804.672 ) 804.688
+		)
+	)
+	shader_names ( 1
+		named_shader ( TexDiff )
+	)
+	texture_filter_names ( 1
+		named_filter_mode ( MipLinear )
+	)
+	points ( 8
+		point ( -5 2.7634 0 )
+		point ( 5 2.7634 0 )
+		point ( 5 2.7634 1609.34 )
+		point ( -5 2.7634 1609.34 )
+		point ( -5 1.7634 0 )
+		point ( 5 1.7634 0 )
+		point ( 5 1.7634 1609.34 )
+		point ( -5 1.7634 1609.34 )
+	)
+	uv_points ( 4
+		uv_point ( 0.000499547 2.58125 )
+		uv_point ( 0.999462 2.58125 )
+		uv_point ( 0.9995 -1.58125 )
+		uv_point ( 0.000537872 -1.58125 )
+	)
+	normals ( 5
+		vector ( -1 0 0 )
+		vector ( 0 0 1 )
+		vector ( 1 0 0 )
+		vector ( 0 0 -1 )
+		vector ( 0 1 0 )
+	)
+	sort_vectors ( 1
+		vector ( 0 2.2634 804.672 )
+	)
+	colours ( 0 )
+	matrices ( 1
+		matrix BOX01 ( 1 0 0 0 1 0 0 0 1 0 0 0 )
+	)
+	images ( 1
+		image ( ImSiding.ace )
+	)
+	textures ( 1
+		texture ( 0 0 0 ff000000 )
+	)
+	light_materials ( 0 )
+	light_model_cfgs ( 1
+		light_model_cfg ( 00000000
+			uv_ops ( 1
+				uv_op_copy ( 1 0 )
+			)
+		)
+	)
+	vtx_states ( 1
+		vtx_state ( 00000000 0 -5 0 00000002 )
+	)
+	prim_states ( 1
+		prim_state Material_#1 ( 00000000 0
+			tex_idxs ( 1 0 ) 0 0 0 0 1
+		)
+	)
+	lod_controls ( 1
+		lod_control (
+			distance_levels_header ( 0 )
+			distance_levels ( 1
+				distance_level (
+					distance_level_header (
+						dlevel_selection ( 2000 )
+						hierarchy ( 1 -1 )
+					)
+					sub_objects ( 1
+						sub_object (
+							sub_object_header ( 00000400 -1 -1 000001d2 000001c4
+								geometry_info ( 10 1 0 30 0 0 1 0 0 0
+									geometry_nodes ( 1
+										geometry_node ( 1 0 0 0 0
+											cullable_prims ( 1 10 30 )
+										)
+									)
+									geometry_node_map ( 1 0 )
+								)
+								subobject_shaders ( 1 0 )
+								subobject_light_cfgs ( 1 0 ) 0
+							)
+							vertices ( 20
+								vertex ( 00000000 0 4 ffffffff ff000000
+									vertex_uvs ( 1 0 )
+								)
+								vertex ( 00000000 1 4 ffffffff ff000000
+									vertex_uvs ( 1 1 )
+								)
+								vertex ( 00000000 2 4 ffffffff ff000000
+									vertex_uvs ( 1 2 )
+								)
+								vertex ( 00000000 3 4 ffffffff ff000000
+									vertex_uvs ( 1 3 )
+								)
+								vertex ( 00000000 4 3 ffffffff ff000000
+									vertex_uvs ( 1 0 )
+								)
+								vertex ( 00000000 5 3 ffffffff ff000000
+									vertex_uvs ( 1 1 )
+								)
+								vertex ( 00000000 1 3 ffffffff ff000000
+									vertex_uvs ( 1 1 )
+								)
+								vertex ( 00000000 0 3 ffffffff ff000000
+									vertex_uvs ( 1 0 )
+								)
+								vertex ( 00000000 5 2 ffffffff ff000000
+									vertex_uvs ( 1 1 )
+								)
+								vertex ( 00000000 6 2 ffffffff ff000000
+									vertex_uvs ( 1 2 )
+								)
+								vertex ( 00000000 2 2 ffffffff ff000000
+									vertex_uvs ( 1 2 )
+								)
+								vertex ( 00000000 1 2 ffffffff ff000000
+									vertex_uvs ( 1 1 )
+								)
+								vertex ( 00000000 6 1 ffffffff ff000000
+									vertex_uvs ( 1 2 )
+								)
+								vertex ( 00000000 7 1 ffffffff ff000000
+									vertex_uvs ( 1 3 )
+								)
+								vertex ( 00000000 3 1 ffffffff ff000000
+									vertex_uvs ( 1 3 )
+								)
+								vertex ( 00000000 2 1 ffffffff ff000000
+									vertex_uvs ( 1 2 )
+								)
+								vertex ( 00000000 7 0 ffffffff ff000000
+									vertex_uvs ( 1 3 )
+								)
+								vertex ( 00000000 4 0 ffffffff ff000000
+									vertex_uvs ( 1 0 )
+								)
+								vertex ( 00000000 0 0 ffffffff ff000000
+									vertex_uvs ( 1 0 )
+								)
+								vertex ( 00000000 3 0 ffffffff ff000000
+									vertex_uvs ( 1 3 )
+								)
+							)
+							vertex_sets ( 1
+								vertex_set ( 0 0 20 )
+							)
+							primitives ( 2
+								prim_state_idx ( 0 )
+								indexed_trilist (
+									vertex_idxs ( 30 0 2 1 2 0 3 4 6 5 6 4 7 8 10 9 10 8 11 12 14 13 14 12 15 16 18 17 18 16 19 )
+									normal_idxs ( 10 4 3 4 3 3 3 3 3 2 3 2 3 1 3 1 3 0 3 0 3 )
+									flags ( 10 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 )
+								)
+							)
+						)
+					)
+				)
+			)
+		)
+	)
+)
+max_data (
+	max_dlev_data (
+		max_smoothing_data ( 10 00000004 00000004 00000001 00000001 00000002 00000002 00000001 00000001 00000002 00000002 0 )
+		hierarchy ( 1 -1 )
+	)
+	matrices ( 1
+		matrix BOX01 ( 1 0 0 0 1 0 0 0 1 0 0 0 )
+	)
+	sub_objects ( 1 0 )
+	max_sort_priorities ( 1 0 )
+)
