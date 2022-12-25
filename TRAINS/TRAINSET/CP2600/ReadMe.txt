c0f61b58784d15bdabc0a48b814e92c01488b3da
CPV <cpvirtual@gmail.com>
2022-12-25 18:24:39 +0000

    add electric locomotive CP2600

---
+++ TRAINS/TRAINSET/CP2600/ReadMe.txt
@@ -0,0 +1,19 @@
+CP série 2600/2620
+
+Versão OR   1.1 22DEZ2022
+
+Technical Data
+	Distance Levels
+	2000m  - =~23600 polygons
+
+==================================================================
+Autores e Contactos
+Modelo:	Nelso Silva - SorefameVirtual ©2013
+		Rui Fonseca - CPvirtual
+Texturas:	Nelso Silva - SorefameVirtual ©2013
+Cab/sons:	Rui Finseca - CPvirtual
+
+www.cpvirtual.org
+cpvirtual@gmail.org
+
+==================================================================
---
+++ TRAINS/TRAINSET/CP2600/Script/CircuitBreaker_IT_old.cs
@@ -0,0 +1,118 @@
+﻿using Orts.Common;
+using Orts.Simulation;
+using Orts.Simulation.Physics;
+using ORTS.Scripting.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Orts.Scripting.Script
+{
+    public class CircuitBreaker_IT : CircuitBreaker
+    {
+        private Timer ClosingTimer;
+        private CircuitBreakerState PreviousState;
+
+        public override void Initialize()
+        {
+            ClosingTimer = new Timer(this);
+            ClosingTimer.Setup(ClosingDelayS());
+
+            SetDriverClosingAuthorization(true);
+        }
+
+        public override void Update(float elapsedSeconds)
+        {
+            SetClosingAuthorization(TCSClosingAuthorization() && CurrentPantographState() == PantographState.Up);
+
+            switch (CurrentState())
+            {
+                case CircuitBreakerState.Closed:
+                    if (!ClosingAuthorization() || DriverOpeningOrder())
+                    {
+                        SetCurrentState(CircuitBreakerState.Open);
+                    }
+                    break;
+
+                case CircuitBreakerState.Closing:
+                    if (ClosingAuthorization() && DriverClosingOrder())
+                    {
+                        if (!ClosingTimer.Started)
+                        {
+                            ClosingTimer.Start();
+                        }
+
+                        if (ClosingTimer.Triggered)
+                        {
+                            ClosingTimer.Stop();
+                            SetCurrentState(CircuitBreakerState.Closed);
+                        }
+                    }
+                    else
+                    {
+                        ClosingTimer.Stop();
+                        SetCurrentState(CircuitBreakerState.Open);
+                    }
+                    break;
+
+                case CircuitBreakerState.Open:
+                    if (ClosingAuthorization() && DriverClosingOrder())
+                    {
+                        SetCurrentState(CircuitBreakerState.Closing);
+                    }
+                    break;
+            }
+
+            if (PreviousState != CurrentState())
+            {
+                switch (CurrentState())
+                {
+                    case CircuitBreakerState.Open:
+                        SignalEvent(Event.CircuitBreakerOpen);
+                        break;
+
+                    case CircuitBreakerState.Closing:
+                        SignalEvent(Event.CircuitBreakerClosing);
+                        break;
+
+                    case CircuitBreakerState.Closed:
+                        SignalEvent(Event.CircuitBreakerClosed);
+                        break;
+                }
+            }
+
+            PreviousState = CurrentState();
+        }
+
+        public override void HandleEvent(PowerSupplyEvent evt)
+        {
+            switch (evt)
+            {
+                case PowerSupplyEvent.CloseCircuitBreakerButtonPressed:
+                    SetDriverOpeningOrder(false);
+                    SetDriverClosingOrder(true);
+                    SignalEvent(Event.CircuitBreakerClosingOrderOn);
+
+                    Confirm(CabControl.CircuitBreakerClosingOrder, CabSetting.On);
+                    if (!ClosingAuthorization())
+                    {
+//                       Message(ConfirmLevel.Warning, Simulator.Catalog.GetString("Circuit breaker closing not authorized"));
+                    }
+                    break;
+                case PowerSupplyEvent.CloseCircuitBreakerButtonReleased:
+                    SetDriverClosingOrder(false);
+                    break;
+                case PowerSupplyEvent.OpenCircuitBreakerButtonPressed:
+                    SetDriverClosingOrder(false);
+                    SetDriverOpeningOrder(true);
+                    SignalEvent(Event.CircuitBreakerClosingOrderOff);
+
+                    Confirm(CabControl.CircuitBreakerClosingOrder, CabSetting.Off);
+                    break;
+                case PowerSupplyEvent.OpenCircuitBreakerButtonReleased:
+                    SetDriverOpeningOrder(false);
+                    break;
+            }
+        }
+    }
+}
